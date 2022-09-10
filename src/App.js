41d8a222c93730e5fda77db21e202ce37af259a8
JoyChudasama <joychudasama11@gmail.com>
2022-09-10 11:00:37 -0300

    removed extra code

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { UserHome } from './pages/UserHome/UserHome';
 function App() {
   return (
     <div className="App">
-      {/* <Login /> */}
-      {/* <SignUp /> */}
+      <SignUp />
+      <Login />
       <UserHome />
     </div>
   );

5d8054899266c7daa0ebfbd90d42fe469bf44a1c
JoyChudasama <joychudasama11@gmail.com>
2022-09-10 14:45:25 -0300

    added firebase, config, and validation for passwordMatch

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <SignUp />
-      <Login />
-      <UserHome />
+      {/* <Login /> */}
+      {/* <UserHome /> */}
     </div>
   );
 }
--- src/pages/Signup/SignUp.jsx
+++ src/pages/Signup/SignUp.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUp.scss';
 import MuiDatePicker from '../../components/MuiComponents/MuiDatePicker';
 import TextField from '@mui/material/TextField';
 import Logo from '../../components/Logo/Logo';
 
 const Register = () => {
+
+    const [password, setPassword] = useState(null);
+    const [confirmPassword, setConfirmPassword] = useState(null);
+    const [isPasswordMatched, setIsPasswordMatched] = useState(true);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const userName = e.target.userName.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const confirmPassword = e.target.confirmPassword.value;
+        const dateOfBirth = document.getElementsByClassName('muiDatePicker')[0].querySelector('input').value;
+    }
+
+    const validatePassword = () => {
+        password !== confirmPassword ? setIsPasswordMatched(false) : setIsPasswordMatched(true);
+    }
 
     return (
         <>
@@ -14,12 +32,21 @@ const Register = () => {
 
                     <span className='title'>Sign Up</span>
 
-                    <form>
+                    <form className="signUpForm" onSubmit={handleSubmit}>
                         <div className='formInputContainer'>
-                            <TextField type='text' label='Username' />
-                            <TextField type='email' label='Email' />
-                            <TextField type='password' label='Password' />
-                            <TextField type='password' label='Confirm Password' />
+                            <TextField type='text' label='Username' name='userName' required={true} />
+                            <TextField type='email' label='Email' name='email' required={true} />
+                            <TextField type='Password' label='Password' name='password' required={true} onChange={(e) => { setPassword(e.target.value) }}
+                                onKeyUp={validatePassword}
+                            />
+
+                            <TextField type='Password' label='Confirm Password' name='confirmPassword' className="confirmPassword" required={true}
+                                onChange={(e) => { setConfirmPassword(e.target.value) }}
+                                onKeyUp={validatePassword}
+                                error={isPasswordMatched ? false : true}
+                                helperText={isPasswordMatched ? '' : "Password does not match"}
+                            />
+
                             <MuiDatePicker label="Date of Birth" />
                         </div>
 

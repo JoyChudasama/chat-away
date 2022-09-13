86590cfe45069abf218ba2985e2254e4ae92d9f6
JoyChudasama <joychudasama11@gmail.com>
2022-09-12 22:12:37 -0300

    updated style for searchResult and fixed handleSearch

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,74 +4,69 @@ import tempAvatar from '../img/avatar.jpg';
 import { fireabseDatabase } from '../firebase';
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { showToast } from '../utils/SweetAlert';
-import { isEmpty } from '@firebase/util';
 import { useEffect } from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 const SearchBar = () => {
 
   const [userName, setUserName] = useState('');
   const [users, setUsers] = useState([]);
-  
-  
-  useEffect(()=>{
-    if(userName.length > 0){
-      try {
-        const searchQuery = query(collection(fireabseDatabase, "users"), where("userName", "==", userName));
-        getUsers(searchQuery).then(users=>{
-          users.forEach(user => {
-              setUsers(prevState => {
-              return  [...prevState, user];
-            })
-          });
-        });
-      } catch (e) {
-        console.log(e)
-        showToast({
-          title: 'Oops..Something went wrong.',
-          position: 'top-end',
-          icon: 'error',
-          timer: 2000,
-          isShowTimeProgressBar: 'error',
-          isShowConfirmButton: false,
-          customClass: 'sweetalert sweetalertToast'
-        });
-      }
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    handleUserSearch();
+    return () => setUsers([]);
+  }, [userName]);
+
+  const handleUserSearch = async () => {
+
+    const searchQuery = query(collection(fireabseDatabase, "users"), where("userName", "==", userName));
+
+    try {
+      const querySnapshot = await getDocs(searchQuery);
+      querySnapshot.forEach(doc => {
+        doc.data().uid !== currentUser.uid && setUsers(prevUsers => [...prevUsers, doc.data()])
+      });
+    } catch (e) {
+      showToast({
+        title: 'Oops..Something went wrong.',
+        position: 'top-end',
+        icon: 'error',
+        timer: 2000,
+        isShowTimeProgressBar: 'error',
+        isShowConfirmButton: false,
+        customClass: 'sweetalert sweetalertToast'
+      });
     }
 
-    // return () => {
-    //   // cancel the subscription
-    //   setUsers([]);
-    // };
-  },[userName]);
-
-  const getUsers = async (searchQuery) => {
-    const querySnapshot = await getDocs(searchQuery);
-
-    return querySnapshot.docs.map(doc => {
-      return doc.data();
-    })
-  }
-  console.log(users)
+  };
 
   return (
     <div className='searchBar'>
-      <div className='searchForm'>
-        <input type='text' placeholder='Search Username' onChange={e => setUserName(e.target.value)} value={userName} />
-      </div>
 
+      <div className='searchForm'>
+        <input type='text' placeholder='Search Username' onChange={e => {
+          setUserName(e.target.value)
+          handleUserSearch()
+        }} />
+
+      </div>
       {
-        users && users.map(user => {
-          <div className='searchResult'>
-            <Avatar className='searchResultProfilePicture' alt="Profile Picture" src={tempAvatar} />
-            <div className='searchResultUserName'>
-              <span>{user.userName}</span>
+        users && users.map(user =>
+          <div className='searchResultContainerWrapper'>
+            <div className='searchResultContainer'>
+              <Avatar className='searchResultProfilePicture' alt="Profile Picture" src={tempAvatar} />
+              <div className='searchResultUserName'>
+                <span>{user.userName}</span>
+              </div>
             </div>
           </div>
-        })
-      }
+        )}
+
 
     </div >
   )
 }
 
-export default SearchBar
+export default SearchBar;
